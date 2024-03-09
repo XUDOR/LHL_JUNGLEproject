72245667c76fbb87b4b25290440b2a0b9d6f4c68
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-09 11:52:27 -0800

    line item

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,11 @@
 class Order < ApplicationRecord
-  
   has_many :line_items
-
+  
   monetize :total_cents, numericality: true
 
   validates :stripe_charge_id, presence: true
 
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
+  end
 end

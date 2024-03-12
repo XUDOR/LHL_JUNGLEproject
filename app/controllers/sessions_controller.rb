49408191a3cbfd4771a96b49bface3d1d9229aff
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-11 19:16:12 -0700

    crud / new for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # User is authenticated
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully!"
+    else
+      # Authentication failed
+      flash.now[:alert] = "Invalid email/password combination"
+      render :new
+    end
   end
 
   def destroy

0a0ceb28570c69c4bfe446fc6d2d0b005945f4c7
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-17 18:10:34 -0700

    post redirect

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,11 +8,17 @@ RSpec.describe "Users", type: :request do
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/users/create"
-      expect(response).to have_http_status(:success)
+  describe "POST /create" do
+    it "redirects to root path after user creation" do
+      post "/users", params: { user: { first_name: "John", last_name: "Doe", email: "john@example.com", password: "password", password_confirmation: "password" } }
+      expect(response).to redirect_to(root_path)
+      
+      # Since it's a redirect, there's no need to follow it for this test
+      # follow_redirect!
+      
+      # After the redirection, you would typically expect a different status,
+      # such as :found (302) or :redirect (303), not :success (200)
+      # expect(response).to have_http_status(:success)
     end
   end
-
 end
